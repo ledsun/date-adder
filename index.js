be62f9ac6a04a32e7ef28d8cc5dd35cc925068de
ledsun <shigeru.nakajima@gmail.com>
2014-12-05 19:11:25 +0900

    Add the distribution file.

---
+++ index.js
@@ -0,0 +1,53 @@
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.dateAdder = factory();
+  }
+}(this, function() {
+'use strict'
+var paramMap = {
+    years: 'FullYear',
+    months: 'Month',
+    days: 'Date',
+    hours: null,
+    minutes: null,
+    seconds: null,
+    milliseconds: null
+  },
+  supported = Object.keys(paramMap),
+  acceptSupported = function(name) {
+    return supported.indexOf(name) !== -1
+  },
+  toPascal = function(name) {
+    return name[0].toUpperCase() + name.substr(1)
+  },
+  toMaterials = function(name, params) {
+    var standardAPI = paramMap[name] || toPascal(name)
+    return {
+      getter: 'get' + standardAPI,
+      setter: 'set' + standardAPI,
+      diff: params[name]
+    }
+  },
+  add = function(date, d) {
+    date[d.setter](date[d.getter]() + d.diff)
+    return date
+  }
+
+function dateAdder(src, params) {
+  src = src || new Date
+  params = params || {}
+
+  return Object.keys(params)
+    .filter(acceptSupported)
+    .map(function(name) {
+      return toMaterials(name, params)
+    })
+    .reduce(add, new Date(+src))
+}
+
+return dateAdder;
+}));
